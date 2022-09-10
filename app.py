1cecd769e6c74eeded0867eb13bd459ab80b8241
shashankkumarverma <ds.shashankkumarverma@gmail.com>
2022-09-10 12:23:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+"""app.py"""
+
+__author__="rajesh"
+__copyright__ ="xyz"
+__created_date__= "30-04-2022"
+
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import config as cfg
+
+app = Flask(__name__)
+cors = CORS(app)
+
+# GET === whenever you have to fetch something from DB
+# POST/PATCH === Insert the new item in DB
+# Delete === if we have to remove the data
+# put === to update something
+
+
+@app.route("/",methods=["GET","POST"])
+@cross_origin(origins="*")
+def hello_world():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True,port=cfg.port)
+
+
+@app.route("even", methods=["GET", "POST"])
+@cross_origin(origins="*")
+even= [1,2,3,4,5,6,8]
+    return "even"
